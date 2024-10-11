8df62d41762658036d943beb969ae92cb2f13acb
Diego Fernández <Diego Fernndez@DESKTOP-DQI8IBP>
2024-10-11 17:20:25 +0200

    Commit día 11/10/2024

---
+++ src/main/java/com/mycompany/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author Diego Fernández
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
